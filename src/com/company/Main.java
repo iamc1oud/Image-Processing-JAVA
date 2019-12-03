d82e64e572c5be3da68c5700cbc68715cb2d0210
AjjuSingh <ajaykumar.kumar900@gmail.com>
2019-12-03 21:10:44 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int width = 193;
+        int height = 200;
+
+        BufferedImage image = null;
+
+        try {
+            File input_file = new File("C:\\Users\\cloudmax.tech\\Pictures\\35bffa2e59bd2d2fb747048acc7bb6de.jpg");
+            image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+            image = ImageIO.read(input_file);
+            System.out.println(input_file);
+        }
+        catch (IOException e){
+            System.out.println(e);
+        }
+
+        try {
+            File output_file = new File("C:\\Users\\cloudmax.tech\\Pictures\\35bffa2e59bd2d2fb747048acc7bb62de.jpg");
+            ImageIO.write(image, "jpg", output_file);
+            System.out.println("Writing complete");
+        }
+        catch (IOException e){
+            System.out.println(e);
+        }
+    }
+}
